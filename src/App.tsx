621214ab7d2faab55931198508473d9675ebe722
Kirill Alexander Khalitov <voronar@gmail.com>
2018-10-10 20:01:44 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Form from 'components/Form';
+import React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Form />
+      </div>
+    );
+  }
+}
+
+export default App;
